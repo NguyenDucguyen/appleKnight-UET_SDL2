9bc32c51f741e81d493edc44075f87942bb79bd0
NguyenDucguyen <nguyenhaiabatrung05@gmail.com>
2024-04-08 22:00:09 +0800

    message

---
+++ Math.h
@@ -0,0 +1,14 @@
+#ifndef MATH_H_
+#define MATH_H_
+ class Math {
+public:
+	static float Clamp(float value, float min, float max) {
+		if (value < min)
+			return min;
+		if (value > max)
+			return max;
+		return value;
+	}
+};
+
+#endif
